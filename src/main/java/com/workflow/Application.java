c4c584296e6bcb58fe234decec2342e88b9d9277
bally_dev <prabal_sinha2000@yahoo.com>
2023-06-09 22:05:55 +0530

    OpenAPI, POSTGRES Config.

--- src/main/java/com/workflow/Application.java
+++ src/main/java/com/workflow/Application.java
@@ -7,7 +7,10 @@ import io.swagger.v3.oas.annotations.info.*;
 @OpenAPIDefinition(
     info = @Info(
             title = "micronaut-camunda",
-            version = "0.0"
+            version = "0.0",
+            description = "My API",
+            license = @License(name = "Apache 2.0", url = "https://github.com/Bally26"),
+            contact = @Contact(url = "https://github.com/Bally26", name = "Prabal", email = "prabals933631@gmail.com")
     )
 )
 public class Application {
